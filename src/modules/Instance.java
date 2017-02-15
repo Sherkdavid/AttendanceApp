9c69f8d6db2f67476dfdbbf620a42d7d7ee581db
David Murphy <dmurphy10@mycit.ie>
2017-02-15 20:27:43 +0000

    Java classes updated

---
+++ src/modules/Instance.java
@@ -0,0 +1,12 @@
+package modules;
+import users.*;
+public class Instance{
+	Lecturer lecturer;
+	String instance_id, title;
+	Module module;
+	int lecture_count;
+	public Instance(String instance_id, String title, Module mod)
+	{
+		
+	}
+}
